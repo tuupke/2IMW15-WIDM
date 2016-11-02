aff11d2136a7e3fe591bf68ae72e3b615d55e8b1
Mart Pluijmaekers <mart@tuupke.nl>
2016-11-02 12:33:05 +0100

    Changes on Something.py

--- Something.py
+++ Something.py
@@ -2,6 +2,12 @@ import csv
 import nltk
 from sklearn.feature_extraction.text import *
 import re
+import MySQLdb
+
+cnx = MySQLdb.connect(user='root', passwd='',
+             host='127.0.0.1', db='wirdm', charset="utf8mb4")
+
+cursor = cnx.cursor()
 
 verb_noun = []
 
@@ -74,40 +80,42 @@ sentiment = [x for x in sentiment if x not in verb_noun]
 
 tweetArray = []
 
-with open('tweets.csv', newline='', encoding='UTF-8') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-     for row in spamreader:
-         if row[3].find('?') == -1:
-             row[3] = " ".join(filter(lambda x: x[0] != '#', row[3].split()))
-             row[3] = " ".join(filter(lambda x: x[0] != '@', row[3].split()))
-             row[3] = " ".join(filter(lambda x: x[0] != 'http/', row[3].split()))
-             row[3] = " ".join(filter(lambda x: x[0: 2] != 'RT', row[3].split()))
-             row[3] = " ".join(filter(lambda x: x[0: 4] != 'http', row[3].split()))
-             row[3] = " ".join(filter(lambda x: x[0: 3] != '"RT', row[3].split()))
+cursor.execute("select * from tweets")
 
-            #filter out question words
-             if row[3].lower().find('what') != -1:
-                 continue
-             if row[3].lower().find('where') != -1:
-                 continue
-             if row[3].lower().find('how') != -1:
-                 continue
-             if row[3].lower().find('when') != -1:
-                 continue
-             if row[3].lower().find('why') != -1:
-                 continue
-             if row[3].lower().find('who') != -1:
-                 continue
-             if row[3].lower().find('which') != -1:
-                 continue
-             if row[3].lower().find('whether') != -1:
-                 continue
-             if row[3].lower().find('if') != -1:
-                 continue
+for row in cursor.fetchall():
+
+    tweet = row[1]
+
+    if tweet.find('?') == -1:
+
+        filtered = re.replace("(^(((\"|'|\s+)?RT:?\s*)))|(http[^\s]+)|(@[^\s]+\s?)|#", tweet).lower()
+
+       #filter out question words
+        if filtered.find('what') != -1:
+            continue
+        if filtered.find('where') != -1:
+            continue
+        if filtered.find('how') != -1:
+            continue
+        if filtered.find('when') != -1:
+            continue
+        if filtered.find('why') != -1:
+            continue
+        if filtered.find('who') != -1:
+            continue
+        if filtered.find('which') != -1:
+            continue
+        if filtered.find('whether') != -1:
+            continue
+        if filtered.find('if') != -1:
+            continue
+
+        print(tweet)
+        tweetArray.append(tweet)
 
 
-             #print(row[3])
-             tweetArray.append(row[3])
+import sys
+sys.exit()
 
 #removing sentiment and emoticon from tweets and cleaning out urls
 wrong = []
